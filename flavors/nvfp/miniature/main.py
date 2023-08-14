bccbf2c5feac7b6a9fd3305d568bbc76c4be4f06
Nicholas Valentinus <nvfastplease@gmail.com>
2023-08-14 04:59:15 -0700

    update miniature design

--- flavors/nvfp/miniature/main.py
+++ flavors/nvfp/miniature/main.py
@@ -3,18 +3,14 @@
 def main(banner_path, banner_alt):
 
     if banner_path != '':
-        header = (
-            f"![{banner_alt}]({banner_path})\n\n"
-            "_OWNER_'s repos (_LINESROUND_ lines of code, _CMIT_ commits, _CHARSAPPROX_ chars)"
-        )
+        header = f'![{banner_alt}]({banner_path})\n\n'
     else:
-        header = (
-            "_OWNER_'s repos (_LINESROUND_ lines of code, _CMIT_ commits, _CHARSAPPROX_ chars)"
-        )
+        header = ''
+    header += "_OWNER_'s repos (_LINESFMT_ lines of code, _CMITFMT_ commits, _CHARSFMT_ chars)"
 
     OPTIONS = {
         'HEADER': header,
-        'FOOTER': '*last update: _DATE_ - _CREDIT_*',
+        'FOOTER': '<sub>last update: _DATE_ - _CREDIT_</sub>',
         'CARD_ORDER': 'cmit\n',
         'SHOW_CREDIT': 'false',
     }
