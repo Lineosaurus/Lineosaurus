b1d8cd041e2308f5f1d0ed93d2d89c14665bc2ef
Nicholas Valentinus <nvfastplease@gmail.com>
2024-01-18 01:31:09 -0800

    this is a dev commit: 1 file changed, 1 insertion(+), 1 deletion(-)
     lino_core/engine/update_readme.py | 2 +-
    
    message generated by QWD (github.com/nvfp/qwd)

--- lino_core/engine/update_readme.py
+++ lino_core/engine/update_readme.py
@@ -54,7 +54,7 @@ def update_readme(
 
     if include_last_activity:
         act_list = []
-        for name, ts in last_acts:
+        for name, ts in last_acts.items():
             d1 = datetime.fromtimestamp(ts).astimezone().strftime(random.choice(['%A, %b %-d, %Y, ', '%A, ', '%B %-d, ']))
             d2 = datetime.fromtimestamp(ts).astimezone().strftime(f"%I:%M%p{random.choice([' utc%z', ''])}").lstrip('0')
             # act_list.append(f"{name} ({datetime.fromtimestamp(ts).astimezone().strftime('%A, %b %d, %Y, %I:%M%p utc%z')})")
