eab1bc070fd0ffb4703262b81e5094ce4065fcbd
Nicholas Valentinus <nvfastplease@gmail.com>
2024-01-17 22:19:27 -0800

    this is a dev commit: 1 file changed, 1 insertion(+), 5 deletions(-)
     lino_core/__main__.py | 6 +-----
    
    message generated by QWD (github.com/nvfp/qwd)

--- lino_core/__main__.py
+++ lino_core/__main__.py
@@ -57,13 +57,9 @@ def main():
         'root_user': os.environ['GITHUB_WORKSPACE'],
         'root_action': os.environ['GITHUB_ACTION_PATH'],
         'gh_actor': os.environ['GITHUB_ACTOR'],
-        # 'act_ver': os.environ['ACTION_REF'],  # the Action version
+        'act_ver': os.environ['GITHUB_WORKFLOW_REF'].split('/')[-1],  # the Action version
     }
     for k,v in misc.items(): print(f"DEBUG: (misc constants) {k}: {repr(v)}")
-    print('-------------------------------------')
-    for k,v in os.environ.items():
-        print(f"{k}: >> {v} <<")
-    print('-------------------------------------')
 
     result = subprocess.run(f"gh repo list {misc['gh_actor']} --visibility public --json url", stdout=subprocess.PIPE, shell=True, text=True)
     repo_list = json.loads(result.stdout)
